d34b7d17f9e25209d26357a3f8a4e8563d394e56
ripathak <ripathak@vmware.com>
2022-09-22 18:14:32 +0530

    Post Handler changes

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -19,14 +19,14 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 		winners, err := data.ListAllJSON()
 		if err != nil {
 			res.WriteHeader(http.StatusInternalServerError)
-			//return
+			return
 		}
 		res.Write(winners)
 	} else {
 		filteredWinners, err := data.ListAllByYear(year)
 		if err != nil {
 			res.WriteHeader(http.StatusBadRequest)
-			//return
+			return
 		}
 		res.Write(filteredWinners)
 	}
@@ -35,6 +35,17 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
+	accessToken := req.Header.Get("X-ACCESS-TOKEN")
+	if isTokenValid := data.IsAccessTokenValid(accessToken); !isTokenValid {
+		res.WriteHeader(http.StatusUnauthorized)
+	} else {
+		err := data.AddNewWinner(req.Body)
+		if err != nil {
+			res.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
+		res.WriteHeader(http.StatusCreated)
+	}
 
 }
 
